c1448d66b77d8901c18eded60a4ea6925cf000cd
AndresVlz2020 <santyguitar6@gmail.com>
2026-05-25 12:18:55 -0500

    Botones, estilos

---
+++ src/shared/components/Button.jsx
@@ -0,0 +1,49 @@
+// Componente button
+
+export default function Button ({
+    variant = "primary",
+    size = "md",
+    type = "button",
+    children,
+    ...props
+}){
+
+    const variants = { 
+        primary: "bg-brand text-inverse hover:bg-brand-hover",
+        secondary: "border border-brand bg-brand-soft text-primary hover:bg-brand-soft-hover"
+    };
+
+    const sizes = {
+        sm: `
+        h-8
+        px-4 
+        before:absolute before:content-['']
+        before:-inset-y[9px] before:content-x-[0px]
+        
+        `,
+        md: "h-10 px-6 before:absolute before:content-[''] before:-inset-y-[4px]before:-insent-x-[0px] "
+
+
+    }
+
+    
+    return(
+
+        <button
+            type = {type}
+            className={`
+            relative
+            inline-flex items-center justify-center
+            rounded-md
+            transition-colors
+            ${variants[variant]}
+            ${sizes[size]}
+            `}
+            {...props}
+        
+            >
+            {children}
+        </button>
+
+    )
+}
--- src/shared/components/Input.jsx
+++ src/shared/components/Input.jsx
@@ -1,21 +1,60 @@
 export default function Input({
     label,
     htmlFor,
-    type = "text",
+    type= "text",
+    variant= "primary",
+    size= "md",
     ...props
-}) {
-    return (
+}){
+
+    // Obligatorio que estos valores tienen que ser con variables
+    const variants = {
+        primary:`
+        border-brand
+        bg-background
+        `,
+        secondary:`
+        border-red-950
+        `,
+        tertiary:`
+        border-green-950
+        `,
+    }
+    const sizes = {
+        sm:`
+        h-8
+        `,
+        md:`
+        h-10
+        `,
+        lg:`
+       h-12
+        `,
+    }
+
+    
+    return(
         <div>
+
             {/* Label */}
-            <label
+            <label 
+            // htmlFor con kebab-case
                 htmlFor={htmlFor}
-                className="
+                className={`
                     block
                     text-caption
                     mb-1
                     text-secondary
-                "
-            >
+                    ${
+                      size === "sm" 
+                      ? "-mb-2" 
+                      : size === "md" 
+                      ? "-mb-0" 
+                      : "mb-1"
+                    }
+                `} 
+                
+                >
                 {label}
             </label>
 
@@ -23,31 +62,44 @@ export default function Input({
             <div
                 className="
                     relative
-                    h-10
+                    h-12
                     flex
                     items-center
                 "
-            >
-                <input
-                    id={htmlFor}
-                    type={type}
+                
+                >
+                    {/* Área interactiva invisible (48px) */}
+                <div
                     className="
+                        absolute inset-0
+                    "
+                    onMouseDown={(e)=>{
+                        e.preventDefault();
+                        e.currentTarget.focus();
+                    }}
+                    />
+                    
+                    {/* Input visual */}
+                    <input
+                        id={htmlFor}
+                        type={type}
+                        className={`
                         relative
-                        w-full
-                        h-10
+                        w-80
                         rounded-md
                         border
-                        border-black
-                        bg-white
                         px-4
                         text-body
                         focus:outline-none
                         focus:ring-2
-                        focus:ring-primary
-                    "
-                    {...props}
-                />
+                        focus:ring-ring
+                        focus:ring-brand
+                        ${variants[variant]}
+                        ${sizes[size]}
+                        `}
+                        {...props}
+                    />
             </div>
         </div>
-    );
+    )
 }
